e9cdca53683e482d83227d7b35a9659e94cc4b38
Miles Christensen <9355108+mdc32@users.noreply.github.com>
2022-04-20 14:12:38 -0700

    cleanup index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ReduxWrapper from './ReduxWrapper';
 import reportWebVitals from './reportWebVitals';
 
+import { Provider } from 'react-redux';
+
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore'; 
+import { firebaseReducer, ReactReduxFirebaseProvider } from 'react-redux-firebase';
+import { createFirestoreInstance, firestoreReducer } from 'redux-firestore';
+import { configureStore } from '@reduxjs/toolkit';
+
+import TestComponent from './TestComponent';
+
+const store = configureStore({
+  reducer: {
+    firebase: firebaseReducer,
+    firestore: firestoreReducer, // <- needed if using firestore
+  }
+});
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDs9yo12K8YT79DyMVEguJ25hO0d9V-JwQ",
+  authDomain: "cs124-lab3-d845a.firebaseapp.com",
+  projectId: "cs124-lab3-d845a",
+  storageBucket: "cs124-lab3-d845a.appspot.com",
+  messagingSenderId: "204426368246",
+  appId: "1:204426368246:web:c8c15574b455c27c0448d7"
+};
+
+// react-redux-firebase config
+const rrfConfig = {
+  userProfile: 'users',
+  useFirestoreForProfile: true
+}
+
+// Initialize firebase instance and firestore
+firebase.initializeApp(firebaseConfig);
+firebase.firestore();
+
+const rrfProps = {
+  firebase,
+  config: rrfConfig,
+  dispatch: store.dispatch,
+  createFirestoreInstance // <- needed if using firestore
+}
+
 ReactDOM.render(
-    <ReduxWrapper />,
+    <Provider store={store}>
+      <ReactReduxFirebaseProvider {...rrfProps}>
+        <TestComponent />
+      </ReactReduxFirebaseProvider>
+    </Provider>,
   document.getElementById('root')
 );
 
