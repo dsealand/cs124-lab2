736d0e1e9d1dc49585fd4ba7178037d11ad74e08
Miles Christensen <9355108+mdc32@users.noreply.github.com>
2022-05-06 17:33:37 -0700

    add constants

---
+++ src/constants.js
@@ -0,0 +1,19 @@
+const TABS_COLLECTION = "tabs-1";
+const TASKS_COLLECTION = "tasks";
+const SHARED_USERS = "sharedUsers";
+
+const ARIA_KEYCODES = [13, 32];
+
+const ERROR_MESSAGES = {
+  "auth/invalid-email": "This email address is not valid. Please try again.",
+  "auth/internal-error": "An error occurred. Please try again.",
+  "auth/user-not-found": "Incorrect email address or password.",
+  "auth/wrong-password": "Incorrect email address or password."
+}
+
+export default {
+  TABS_COLLECTION,
+  TASKS_COLLECTION,
+  ARIA_KEYCODES,
+  ERROR_MESSAGES
+}
